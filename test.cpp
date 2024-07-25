4544cd4ba86c63ed73dd4e130ac49ae2795b2917
Long <dohoanglong1308@gmail.com>
2024-07-25 14:59:22 +0700

    test

---
+++ test.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+// Hàm đếm số chữ số của một số nguyên
+int countDigits(int number) {
+    int count = 0;
+    if (number == 0) {
+        return 1; // Số 0 có 1 chữ số
+    }
+    while (number > 0) {
+        number /= 10;
+        count++;
+    }
+    return count;
+}
+
+int main() {
+    int N, K;
+    cin >> N >> K;
+
+    int sum = 0;
+    for (int i = 0; i < N; ++i) {
+        int number;
+        cin >> number;
+        if (countDigits(number) == K) {
+            sum += number;
+        }
+    }
+
+    cout << sum << endl;
+
+    return 0;
+}
